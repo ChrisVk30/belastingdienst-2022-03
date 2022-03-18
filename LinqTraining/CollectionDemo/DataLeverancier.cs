c5944a0cac3f40c851d1a1937a6136e29d829e13
JP <jp@jp.nl>
2022-03-18 09:42:43 +0100

    IEnumerable en collections behandeld

---
+++ LinqTraining/CollectionDemo/DataLeverancier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionDemo
+{
+    public class DataLeverancier
+    {
+        public IEnumerable<int> GeefData()
+        {
+            // deelresultaat  partial result
+            Console.WriteLine("eerste");
+            yield return 4;
+            Console.WriteLine("tweede");
+            yield return 8;
+            Console.WriteLine("derde");
+            yield return 15;
+            Console.WriteLine("vierde");
+            yield return 16;
+        }
+    }
+}
---
+++ LinqTraining/CollectionDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CollectionDemo;
+
+Console.WriteLine("Hello, World!");
+
+
+var linked = new LinkedList<int>();
+linked.AddFirst(4);
+
+linked.AddFirst(8);
+linked.AddLast(15);
+
+var derdeItem = linked.Find(15);
+
+linked.AddAfter(derdeItem, 23);
+linked.AddLast(42);
+
+
+foreach (var getal in linked)
+{
+    Console.WriteLine($"getal: {getal}");
+}
+
+// 8 4 15 23 42
+
+/**
+ * IEnumerable<T>: met een loopje erdoor kunnen gaan  .MoveNext()  .Current
+ * ICollection<T>: .Count   .Add() .Remove()
+ * IList<T>      : .InsertAt
+ */
+
+
+var leverancier = new DataLeverancier();
+var data = leverancier.GeefData();
+
+foreach (var item in data)
+{
+    Console.WriteLine($"item: {item}");
+}
+
+
+
+
+
