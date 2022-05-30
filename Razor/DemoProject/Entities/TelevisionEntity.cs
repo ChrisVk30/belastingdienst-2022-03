d939affbd7fd375cabeed2a0f3cb63d448d19d1e
JP <jp@jp.nl>
2022-05-30 13:01:57 +0200

    Formulier met validatie laten zien

--- Razor/DemoProject/Entities/TelevisionEntity.cs
+++ Razor/DemoProject/Entities/TelevisionEntity.cs
@@ -1,19 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DemoProject.Entities
 {
+	// FluentValidations are an interesting alternative
+
 	public class TelevisionEntity
 	{
 		public int Id { get; set; }
 
+		[Required(ErrorMessage = "Vul dit in aub")]
+		[RegularExpression("^[a-zA-Z0-9]+$", ErrorMessage = "Alleen letters en cijfers graag")]
 		public string Brand { get; set; }
 
+		[Required]
 		public decimal Price { get; set; }
-
+		
+		[Required]
 		public decimal Size { get; set; }
 	}
 }
--- Razor/DemoProject/Pages/Index.cshtml.cs
+++ Razor/DemoProject/Pages/Index.cshtml.cs
@@ -4,36 +4,52 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DemoProject.Pages
 {
-    // code-behind
-    public class IndexModel : PageModel
-    {
-        public List<TelevisionEntity> tvs = new()
+	// code-behind
+	public class IndexModel : PageModel
+	{
+		public static List<TelevisionEntity> Televisions { get; set; } = new()
 		{
-            new TelevisionEntity()
+			new TelevisionEntity()
 			{
-                Id = 4,
-                Brand = "Samsung",
-                Size = 65M,
-                Price = 1600M
-            },
-            new TelevisionEntity()
-            {
-                Id = 8,
-                Brand = "LG",
-                Size = 79M,
-                Price = 3600M
-            },
-            new TelevisionEntity()
-            {
-                Id = 16,
-                Brand = "Philips",
-                Size = 42M,
-                Price = 380M
-            }
-        };
+				Id = 4,
+				Brand = "Samsung",
+				Size = 65M,
+				Price = 1600M
+			},
+			new TelevisionEntity()
+			{
+				Id = 8,
+				Brand = "LG",
+				Size = 79M,
+				Price = 3600M
+			},
+			new TelevisionEntity()
+			{
+				Id = 16,
+				Brand = "Philips",
+				Size = 42M,
+				Price = 380M
+			}
+		};
 
-        public void OnGet()
-        {
-        }
-    }
+		[BindProperty]
+		public TelevisionEntity NewTelevision { get; set; } // model binding
+
+
+		public void OnGet()
+		{
+		}
+
+		public void OnPost()
+		{
+			Thread.Sleep(3000);
+			if (!ModelState.IsValid)
+			{
+				return;
+			}
+			NewTelevision.Id = Televisions.Max(x => x.Id) + 1;
+			Console.WriteLine($"Nieuwe tv: {NewTelevision.Brand} is {NewTelevision.Size}\" groot");
+			Televisions.Add(NewTelevision);
+		}
+	}
 }
