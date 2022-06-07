88561aba6f7bdb7e72274d58ed420f7dee79f7d2
JP <jp@jp.nl>
2022-06-07 09:59:01 +0200

    Wat over security verteld

---
+++ Razor/DemoProject/Pages/State.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoProject.Pages
+{
+    public class StateModel : PageModel
+    {
+        public string Text { get; set; } = "<strong>hallo</strong> <script>alert('q');</script>";
+
+        [Authorize]
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPut()
+        {
+
+        }
+    }
+}
