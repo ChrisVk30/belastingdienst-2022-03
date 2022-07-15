24ea1b888f86539c9316cf6edb2b1134be493614
JP <jp@jp.nl>
2022-07-15 13:23:27 +0200

    Classes verder laten zien

--- TypeScript development/demo/dist/classes.js
+++ TypeScript development/demo/dist/classes.js
@@ -1,13 +1,43 @@
 "use strict";
-class Productje {
+var _a;
+class Voorwerp {
     constructor() {
-        // this.id = 143;
-        this.fillId();
+        this.name = 'iets';
     }
-    fillId() {
-        this.id = 4565;
+    doe() {
+        // this.name = 'fsdf';
     }
 }
-let product = new Product();
+class Productje {
+    constructor(id, description, price) {
+        this.id = id;
+        this.description = description;
+        this.price = price;
+        // this.fillId();
+    }
+    fillId() {
+        // this.id = 4565;
+    }
+}
+let product = new Productje(4, 'Iets tofs', 999);
 console.log(product);
+class DeadBeing {
+}
+class DeadAnimal extends DeadBeing {
+    move() {
+        console.log(this);
+    }
+}
+new DeadAnimal();
+// new DeadBeing(); // compile-time only, as multiple things with TypeScript
+let roadkill = new DeadAnimal();
+roadkill.move();
+// TypeScript does not do a lot here
+let move = roadkill.move;
+move();
+// this als een fake parameter
+function handleClick() {
+    console.log(this);
+}
+(_a = document.querySelector('#btn')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', handleClick);
 //# sourceMappingURL=classes.js.map
--- TypeScript development/demo/src/classes.ts
+++ TypeScript development/demo/src/classes.ts
@@ -1,19 +1,57 @@
 
 
-class Productje {
-    id!: number;
-    description!: string;
-    price!: number;
+class Voorwerp {
+    readonly name: string;
 
     constructor() {
-        // this.id = 143;
-        this.fillId();
+        this.name = 'iets';
     }
 
-    fillId() {
-        this.id = 4565;
+    doe() {
+        // this.name = 'fsdf';
     }
 }
 
-let product = new Product();
+class Productje {
+    constructor(
+        private readonly id: number,
+        private description: string,
+        private price: number) {
+        // this.fillId();
+    }
+
+    fillId() {
+        // this.id = 4565;
+    }
+}
+
+let product = new Productje(4, 'Iets tofs', 999);
 console.log(product);
+
+
+abstract class DeadBeing {
+
+}
+class DeadAnimal extends DeadBeing {
+    move() {
+        console.log(this);
+    }
+}
+new DeadAnimal();
+// new DeadBeing(); // compile-time only, as multiple things with TypeScript
+
+let roadkill = new DeadAnimal();
+roadkill.move();
+
+// TypeScript does not do a lot here
+let move = roadkill.move;
+move();
+
+
+// this als een fake parameter
+function handleClick(this: HTMLButtonElement) {
+    console.log(this);
+}
+
+document.querySelector('#btn')?.addEventListener('click', handleClick);
+
