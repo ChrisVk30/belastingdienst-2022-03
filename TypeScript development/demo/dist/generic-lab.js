2eed672d289907ae7b9429ba1276dbceb6c59bef
JP <jp@jp.nl>
2022-07-18 11:43:34 +0200

    Namespaces en modules laten zien

---
+++ TypeScript development/demo/dist/generic-lab.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=generic-lab.js.map
---
+++ TypeScript development/demo/dist/namespace-util.js
@@ -0,0 +1,15 @@
+"use strict";
+var InfoSupport;
+(function (InfoSupport) {
+    var Graphics;
+    (function (Graphics) {
+        var Utilities;
+        (function (Utilities) {
+            function getSize() {
+                console.log('wauw');
+            }
+            Utilities.getSize = getSize;
+        })(Utilities = Graphics.Utilities || (Graphics.Utilities = {}));
+    })(Graphics = InfoSupport.Graphics || (InfoSupport.Graphics = {}));
+})(InfoSupport || (InfoSupport = {}));
+//# sourceMappingURL=namespace-util.js.map
