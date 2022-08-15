6bb44f5915b961dfef314ebca5dcc6edbcd05abc
JP <jpeetjuh@hotmail.com>
2022-08-16 01:27:13 +0200

    Eerste opzetje formulier en route met id werkt. Editorconfig toegevoegd voor formatting

--- Oefencase/blauwe-piste/frontend/src/pages/NewGame.svelte
+++ Oefencase/blauwe-piste/frontend/src/pages/NewGame.svelte
@@ -1,1 +1,23 @@
-new game
+<script lang="ts">
+	import { navigateTo } from "svelte-router-spa";
+
+
+	let name: string;
+
+	const start = () => {
+		console.log('name:', name);
+
+		navigateTo('/game/45');
+	};
+</script>
+
+<main>
+	<h1>Nieuw spel</h1>
+
+	<form on:submit|preventDefault={start}>
+		<label for="input-name">Je naam?</label>
+		<input id="input-name" bind:value={name} />
+		<button>Start!</button>
+	</form>
+	{name}
+</main>
